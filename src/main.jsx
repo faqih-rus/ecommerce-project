c03893eff6de8072c433c6cd3f586dba6d6e9018
faqih-unj1 <faqihsetiawan17@gmail.com>
2023-11-10 20:51:52 +0700

    Revisi Lokasi Css karena gak kedetek asu

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './dist/css/Main.css';
+import './dist/main.css';
 
 import {BrowserRouter} from 'react-router-dom'
 import ScrollToTop from './components/ScrollToTop';
